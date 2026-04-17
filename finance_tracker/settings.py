e0dfc15e676acbd2aaf92b5e9305c014147190a7
Seth Olwande <setholwande@Johns-MacBook-Air.local>
2026-04-17 09:48:28 +0300

    Add PostgreSQL support

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -4,6 +4,7 @@ Django settings for finance_tracker project.
 
 from pathlib import Path
 from decouple import config, Csv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,10 +61,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'finance_tracker.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
